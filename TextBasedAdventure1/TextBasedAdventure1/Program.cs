361bbc718e6253acac8fd2cb5a3770af8c65f2af
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2023-02-16 16:22:29 -0500

    Finished Game Functionality

--- TextBasedAdventure1/TextBasedAdventure1/Program.cs
+++ TextBasedAdventure1/TextBasedAdventure1/Program.cs
@@ -1,21 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, you're now trapped in a dungeon. The only way out is through one of these doors.");
-
-static void PlayerChoice()
+﻿public class Program
 {
-    Console.WriteLine("Two doors stand in front of you. Please enter 1 or 2 in order to choose one");
-    string Choice = Console.ReadLine();
-    GenerateDoor();
-}
 
-static (bool, bool) GenerateDoor()
-{
-    Random random = new Random();
-    int test = random.Next(0, 1);
-    if (test == 0)
+    public static void Main()
+        {
+        Console.WriteLine("Hello, you're now trapped in a dungeon. The only way out is through one of these doors.");
+        int nonDeadEndDoorsOpened = 0;
+
+            while (nonDeadEndDoorsOpened != 5)
+            {
+                bool isNonDoorDeadEnd = PlayerChoice();
+
+                if (isNonDoorDeadEnd)
+                {
+                    Console.WriteLine("The door chosen was the correct door!");
+                    nonDeadEndDoorsOpened++;
+                }
+                else
+                {
+                    Console.WriteLine("The door chosen was a dead end...try again...");
+                }
+            }
+
+            Console.WriteLine("You reached the exit, congrats!");
+        }
+
+    static bool PlayerChoice()
     {
-        return (true, false);
-    }
-    return (false, true);
-}
+        Console.WriteLine("Two doors stand in front of you. Please enter 1 or 2 in order to choose one");
+        string Choice = Console.ReadLine();
+        (bool leftDoor, bool rightDoor) = GenerateDoor();
 
+        if (int.Parse(Choice) == 1)
+        {
+            return leftDoor;
+        }
+        else
+        {
+            return rightDoor;
+        }
+    }
+
+    static (bool, bool) GenerateDoor()
+    {
+        Random random = new Random();
+        int test = random.Next(0, 2);
+        if (test == 0)
+        {
+            return (true, false);
+        }
+        return (false, true);
+    }
+
+}
