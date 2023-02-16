df26b4f47fc2a606dfd1a388de0c8c2511c6e667
Tommy-Nguyen <75864631+Tommy-Nguyen-cpu@users.noreply.github.com>
2023-02-16 16:08:45 -0500

    Began TestDoorMethod
    Began method that tests player choice method and door generation method.

--- TextBasedAdventure1/TextGameUnitTests/UnitTest1.cs
+++ TextBasedAdventure1/TextGameUnitTests/UnitTest1.cs
@@ -6,6 +6,48 @@ namespace TextGameUnitTests
     [TestClass]
     public class UnitTest1
     {
+        // Example generation method. Should be removed once actual method has been implemented.
+        public (bool,bool) generatedoor()
+        {
+            // TODO: Generate random values.
+            return (false, true);
+        }
+
+        // Example player choice method. Should be removed once actual method has been implemented.
+        public bool PlayerChoice(int doorChoice)
+        {
+
+            return true;
+        }
+
+        /// <summary>
+        /// Tests the door generation method.
+        /// 1 door should be obstacle and 1 door should be 
+        /// </summary>
+        [TestMethod]
+        public void TestDoorsGeneration()
+        {
+            // TODO: Replace with actual door generation method signature.
+            (bool leftDoor, bool rightDoor) = generatedoor();
+
+
+            // Checks to see that leftDoor and rightDoor ARE NOT the same value.
+            Assert.IsTrue(leftDoor != rightDoor);
+        }
+
+        [TestMethod]
+        public void TestPlayerChoice()
+        {
+
+            // TODO: replae with actual player choice method signature.
+            bool result = PlayerChoice(1);
+            Assert.IsTrue(result != null);
+
+        }
+
+        /// <summary>
+        /// Tests method responsible for result after player chooses a door.
+        /// </summary>
         [TestMethod]
         public void TestMethod1()
         {
