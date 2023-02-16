150618f4e8cd55e047ef3b3c0acc9ebda1c3d5e2
tranh22atwit <55092475+tranh22atwit@users.noreply.github.com>
2023-02-16 16:08:01 -0500

    First Commit
    First iteration

--- TextBasedAdventure1/TextBasedAdventure1/Program.cs
+++ TextBasedAdventure1/TextBasedAdventure1/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, you're now trapped in a dungeon. The only way out is through one of these doors.");
+
+static void PlayerChoice()
+{
+    Console.WriteLine("Two doors stand in front of you. Please enter 1 or 2 in order to choose one");
+    string Choice = Console.ReadLine();
+    GenerateDoor();
+}
+
+static (bool, bool) GenerateDoor()
+{
+    Random random = new Random();
+    int test = random.Next(0, 1);
+    if (test == 0)
+    {
+        return (true, false);
+    }
+    return (false, true);
+}
+
